797af0c70b640ee0738a5824b7f720e17641fc5a
ygwang <yuanguang.m.wang@outlook.com>
2023-02-25 13:34:35 +0800

    filterruleparam

--- Spotlight.Selection/FilterRuleParameter.cs
+++ Spotlight.Selection/FilterRuleParameter.cs
@@ -9,11 +9,29 @@ namespace Spotlight.Selection
         private FilterRuleParameter() { }
 
         [NodeCategory("Create")]
-        public static ADDB.FilterRule CreateContainsRule(ADDB.ElementId elementId, string value, bool caseSensitive)
+        public static ADDB.FilterRule CreateContainsRule(ADDB.BuiltInParameter builtInParameter, string value, bool caseSensitive)
         {
+            ADDB.ElementId elementId = new ADDB.ElementId(builtInParameter);
             ADDB.FilterRule filterRule = ADDB.ParameterFilterRuleFactory.CreateContainsRule(elementId, value, caseSensitive);
             return filterRule;
         }
 
+        [NodeCategory("Create")]
+        public static ADDB.FilterRule CreateBeginsWithRule(ADDB.BuiltInParameter builtInParameter, string value, bool caseSensitive)
+        {
+            ADDB.ElementId elementId = new ADDB.ElementId(builtInParameter);
+            ADDB.FilterRule filterRule = ADDB.ParameterFilterRuleFactory.CreateBeginsWithRule(elementId, value, caseSensitive);
+            return filterRule;
+        }
+        
+        [NodeCategory("Create")]
+        public static ADDB.FilterRule CreateEndsWithRule(ADDB.BuiltInParameter builtInParameter, string value, bool caseSensitive)
+        {
+            ADDB.ElementId elementId = new ADDB.ElementId(builtInParameter);
+            ADDB.FilterRule filterRule = ADDB.ParameterFilterRuleFactory.CreateEndsWithRule(elementId, value, caseSensitive);
+            return filterRule;
+        } 
+        
+        
     }
 }
