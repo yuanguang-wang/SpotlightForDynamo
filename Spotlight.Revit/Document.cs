f946c64ed7e28ed84707d8d4d82f10352e182da0
ygwang <yuanguang.m.wang@outlook.com>
2023-03-17 23:51:41 +0800

    document.delete

--- Spotlight.Revit/Document.cs
+++ Spotlight.Revit/Document.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Dynamo.Graph.Nodes;
 
 using RevitServices.Persistence;
 using RevitServices.Transactions;
 
 using ADDB = Autodesk.Revit.DB;
+using DYDB = Revit.Elements;
 
 namespace Spotlight.Revit
 {
@@ -20,15 +22,83 @@ namespace Spotlight.Revit
         }
         
         [NodeCategory("Actions")]
-        public static ICollection<ADDB.ElementId> Delete(ADDB.ElementId elementId) 
+        public static ICollection<ADDB.ElementId> Delete(ADDB.ElementId elementId, bool run)
         {
-            TransactionManager.Instance.EnsureInTransaction(CurrentDoc);
+            if (run)
+            {
+                TransactionManager.Instance.EnsureInTransaction(CurrentDoc);
 
-            ICollection<ADDB.ElementId> deletedIdList = CurrentDoc.Delete(elementId);
+                ICollection<ADDB.ElementId> deletedIdList = CurrentDoc.Delete(elementId);
             
-            TransactionManager.Instance.TransactionTaskDone();
+                TransactionManager.Instance.TransactionTaskDone();
 
-            return deletedIdList;
+                return deletedIdList; 
+            }
+            return null;
+        }
+               
+        [NodeCategory("Actions")]
+        public static ICollection<ADDB.ElementId> Delete(ICollection<ADDB.ElementId> elementIds, bool run)
+        {
+            if (run)
+            {
+                TransactionManager.Instance.EnsureInTransaction(CurrentDoc);
+
+                ICollection<ADDB.ElementId> deletedIdList = CurrentDoc.Delete(elementIds);
+            
+                TransactionManager.Instance.TransactionTaskDone();
+
+                return deletedIdList; 
+            }
+            return null;
+        }
+        
+        [NodeCategory("Actions")]
+        public static ICollection<ADDB.ElementId> Delete(DYDB.Element dynamoElement, bool run)
+        {
+            if (run)
+            {
+                TransactionManager.Instance.EnsureInTransaction(CurrentDoc);
+
+                ADDB.ElementId elementId = dynamoElement.InternalElement.Id;
+                ICollection<ADDB.ElementId> deletedIdList = CurrentDoc.Delete(elementId);
+            
+                TransactionManager.Instance.TransactionTaskDone();
+
+                return deletedIdList; 
+            }
+            return null;
+        } 
+        
+                       
+        [NodeCategory("Actions")]
+        public static ICollection<ADDB.ElementId> Delete(ICollection<DYDB.Element> dynamoElements, bool run)
+        {
+            if (run)
+            {
+                TransactionManager.Instance.EnsureInTransaction(CurrentDoc);
+
+                ICollection<ADDB.ElementId> elementIds = dynamoElements.Select(t => t.InternalElement.Id).ToList();
+                ICollection<ADDB.ElementId> deletedIdList = CurrentDoc.Delete(elementIds);
+            
+                TransactionManager.Instance.TransactionTaskDone();
+
+                return deletedIdList; 
+            }
+            return null;
+        }
+
+        [NodeCategory("Actions")]
+        public static void Regenerate(bool run)
+        {
+            if (run)
+            {
+                TransactionManager.Instance.EnsureInTransaction(CurrentDoc);
+
+                CurrentDoc.Regenerate();
+            
+                TransactionManager.Instance.TransactionTaskDone(); 
+            }
         }
 
 
