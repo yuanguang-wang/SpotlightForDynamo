1d4dca34fde962ac18e136a75f71a1d0276df58a
ygwang <yuanguang.m.wang@outlook.com>
2023-03-26 16:31:49 +0800

    filterrule done

--- Spotlight.Selection/FilterRule.cs
+++ Spotlight.Selection/FilterRule.cs
@@ -11,22 +11,62 @@ namespace Spotlight.Selection
         private FilterRule() { }
 
         [NodeCategory("Create")]
-        public static ADDB.FilterCategoryRule FilterCategoryRule(IEnumerable<ADDB.BuiltInCategory> builtInCategories)
+        public static ADDB.FilterRule FilterCategoryRule(IEnumerable<ADDB.BuiltInCategory> builtInCategories)
         {
             ICollection<ADDB.ElementId> elementIds = builtInCategories.Select(category => new ADDB.ElementId(category)).ToList();
-            ADDB.FilterCategoryRule filterCategoryRule = new ADDB.FilterCategoryRule(elementIds);
+            ADDB.FilterRule filterCategoryRule = new ADDB.FilterCategoryRule(elementIds);
             return filterCategoryRule;
         }
 
         [NodeCategory("Create")]
-        public static ADDB.FilterRule FilterDoubleRule(ADDB.FilterableValueProvider valueProvider,
-            ADDB.FilterNumericRuleEvaluator filterNumericRuleEvaluator, double ruleValue, double tolerance)
+        public static ADDB.FilterRule FilterDoubleRule(ADDB.BuiltInParameter builtInParameter, ADDB.FilterNumericRuleEvaluator numericRuleType, double value, double tolerance)
         {
-            ADDB.FilterRule filterRule= new ADDB.FilterDoubleRule(valueProvider, filterNumericRuleEvaluator, ruleValue, tolerance);
+            ADDB.FilterableValueProvider valueProvider = new ADDB.ParameterValueProvider( new ADDB.ElementId(builtInParameter));
+            ADDB.FilterRule filterRule= new ADDB.FilterDoubleRule(valueProvider, numericRuleType, value, tolerance);
             return filterRule;
         }
-        
-        
+       
+        [NodeCategory("Create")]
+        public static ADDB.FilterRule FilterIntegerRule(ADDB.BuiltInParameter builtInParameter, ADDB.FilterNumericRuleEvaluator numericRuleType, int value)
+        {
+            ADDB.FilterableValueProvider valueProvider = new ADDB.ParameterValueProvider( new ADDB.ElementId(builtInParameter));
+            ADDB.FilterRule filterRule= new ADDB.FilterIntegerRule(valueProvider, numericRuleType, value);
+            return filterRule;
+        } 
+              
+        [NodeCategory("Create")]
+        public static ADDB.FilterRule FilterElementIdRule(ADDB.BuiltInParameter builtInParameter, ADDB.FilterNumericRuleEvaluator numericRuleType, ADDB.ElementId value)
+        {
+            ADDB.FilterableValueProvider valueProvider = new ADDB.ParameterValueProvider( new ADDB.ElementId(builtInParameter));
+            ADDB.FilterRule filterRule= new ADDB.FilterElementIdRule(valueProvider, numericRuleType, value);
+            return filterRule;
+        }  
+                      
+        [NodeCategory("Create")]
+        public static ADDB.FilterRule FilterStringRule(ADDB.BuiltInParameter builtInParameter, ADDB.FilterStringRuleEvaluator stringRuleType, string value)
+        {
+            ADDB.FilterableValueProvider valueProvider = new ADDB.ParameterValueProvider( new ADDB.ElementId(builtInParameter));
+            ADDB.FilterRule filterRule= new ADDB.FilterStringRule(valueProvider, stringRuleType, value);
+            return filterRule;
+        }
+
+        [NodeCategory("Create")]
+        public static ADDB.FilterRule FilterHasValueRule(ADDB.BuiltInParameter builtInParameter)
+        {
+            return new ADDB.HasValueFilterRule(new ADDB.ElementId(builtInParameter));
+        }
+
+        [NodeCategory("Create")]
+        public static ADDB.FilterRule FilterHasNoValueRule(ADDB.BuiltInParameter builtInParameter)
+        {
+            return new ADDB.HasNoValueFilterRule(new ADDB.ElementId(builtInParameter));
+        }
+
+        [NodeCategory("Create")]
+        public static ADDB.FilterRule FilterInvertRule(ADDB.FilterRule filterRule)
+        {
+            return new ADDB.FilterInverseRule(filterRule);
+        }
     }
 
 }
--- Spotlight.Selection/RuleType.cs
+++ Spotlight.Selection/RuleType.cs
@@ -9,34 +9,83 @@ namespace Spotlight.Selection
         private RuleType() { }
 
         [NodeCategory("Create")]
-        public static ADDB.FilterNumericEquals NumericEqualsRule()
+        public static ADDB.FilterNumericRuleEvaluator NumericEqualsRule()
         {
             return new ADDB.FilterNumericEquals();
         }
 
         [NodeCategory("Create")]
-        public static ADDB.FilterNumericGreater NumericGreaterRule()
+        public static ADDB.FilterNumericRuleEvaluator NumericGreaterRule()
         {
             return new ADDB.FilterNumericGreater();
         }
 
         [NodeCategory("Create")]
-        public static ADDB.FilterNumericLess NumericLessRule()
+        public static ADDB.FilterNumericRuleEvaluator NumericLessRule()
         {
             return new ADDB.FilterNumericLess();
         }
 
         [NodeCategory("Create")]
-        public static ADDB.FilterNumericGreaterOrEqual NumericGreaterOrEqualRule()
+        public static ADDB.FilterNumericRuleEvaluator NumericGreaterOrEqualRule()
         {
             return new ADDB.FilterNumericGreaterOrEqual();
         }
 
         [NodeCategory("Create")]
-        public static ADDB.FilterNumericLessOrEqual NumericLessOrEqualRule()
+        public static ADDB.FilterNumericRuleEvaluator NumericLessOrEqualRule()
         {
             return new ADDB.FilterNumericLessOrEqual();
         }
 
+        [NodeCategory("Create")]
+        public static ADDB.FilterStringRuleEvaluator StringBeginsWithRule()
+        {
+            return new ADDB.FilterStringBeginsWith();
+        }
+
+        [NodeCategory("Create")]
+        public static ADDB.FilterStringRuleEvaluator StringEndsWithRule()
+        {
+            return new ADDB.FilterStringEndsWith();
+        }
+
+        [NodeCategory("Create")]
+        public static ADDB.FilterStringRuleEvaluator StringContainsRule()
+        {
+            return new ADDB.FilterStringContains();
+        }
+
+        [NodeCategory("Create")]
+        public static ADDB.FilterStringRuleEvaluator StringEqualsRule()
+        {
+            return new ADDB.FilterStringEquals();
+        }
+
+        [NodeCategory("Create")]
+        public static ADDB.FilterStringRuleEvaluator StringGreaterRule()
+        {
+            return new ADDB.FilterStringGreater();
+        }
+
+        [NodeCategory("Create")]
+        public static ADDB.FilterStringRuleEvaluator StringGreaterOrEqualRule()
+        {
+            return new ADDB.FilterStringGreaterOrEqual();
+        }
+
+        [NodeCategory("Create")]
+        public static ADDB.FilterStringRuleEvaluator StringLessRule()
+        {
+            return new ADDB.FilterStringLess();
+        }
+
+        [NodeCategory("Create")]
+        public static ADDB.FilterStringRuleEvaluator StringLessOrEqualRule()
+        {
+            return new ADDB.FilterStringLessOrEqual();
+        }
+
+        
     }
 }
