112c792b55a3a1de3cdf838c941fb419e6d3212b
ygwang <yuanguang.m.wang@outlook.com>
2023-04-01 20:45:29 +0800

    elementslowfilter donw

--- Spotlight.Selection/ElementFilter.cs
+++ Spotlight.Selection/ElementFilter.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Dynamo.Graph.Nodes;
 using ADDB = Autodesk.Revit.DB;
 using SP = Spotlight.Revit;
+using DYDB = Revit.Elements;
 
 // ReSharper disable MemberCanBePrivate.Global
 
@@ -11,40 +12,88 @@ namespace Spotlight.Selection
     {
         private ElementFilter() { }
 
+        // Element Slow Filter //
+        /////////////////////////
+        
         [NodeCategory("Create")]
-        public static ADDB.ElementClassFilter ElementClassFilter(System.Type classType, bool invert = false)
+        public static SP.ElementFilter ElementParameterFilter(ADDB.FilterRule filterRule, bool invert = false)
         {
-            ADDB.ElementClassFilter filter = new ADDB.ElementClassFilter(classType, invert);
-            return filter;
+            return new SP.ElementFilter(new ADDB.ElementParameterFilter(filterRule, invert));
         }
 
         [NodeCategory("Create")]
-        public static ADDB.ElementLevelFilter ElementLevelFilter(ADDB.ElementId levelId, bool invert = false)
+        public static SP.ElementFilter ElementParameterFilter(IList<ADDB.FilterRule> filterRules, bool invert = false)
         {
-            ADDB.ElementLevelFilter filter = new ADDB.ElementLevelFilter(levelId, invert);
-            return filter;
+            return new SP.ElementFilter(new ADDB.ElementParameterFilter(filterRules, invert));
         }
 
         [NodeCategory("Create")]
-        public static ADDB.ElementParameterFilter ElementParameterFilter(ADDB.FilterRule filterRule)
+        public static SP.ElementFilter ElementLevelFilter(DYDB.Level level, bool invert = false)
         {
-            ADDB.ElementParameterFilter elementParameterFilter = new ADDB.ElementParameterFilter(filterRule);
-            return elementParameterFilter;
+            return new SP.ElementFilter(new ADDB.ElementLevelFilter(level.InternalElement.LevelId, invert));
         }
 
         [NodeCategory("Create")]
-        public static ADDB.ElementParameterFilter ElementParameterFilter(IList<ADDB.FilterRule> filterRules)
+        public static SP.ElementFilter ElementLevelFilter(ADDB.Level level, bool invert = false)
         {
-            ADDB.ElementParameterFilter elementParameterFilter = new ADDB.ElementParameterFilter(filterRules);
-            return elementParameterFilter;
+            return new SP.ElementFilter(new ADDB.ElementLevelFilter(level.LevelId, invert));
         }
 
         [NodeCategory("Create")]
-        public static ADDB.ElementParameterFilter ElementParameterFilter(ADDB.FilterRule filterRule, bool inverted)
+        public static SP.ElementFilter ElementLevelFilter(ADDB.ElementId level, bool invert = false)
         {
-            ADDB.ElementParameterFilter elementParameterFilter = new ADDB.ElementParameterFilter(filterRule, inverted);
-            return elementParameterFilter; 
+            return new SP.ElementFilter(new ADDB.ElementLevelFilter(level, invert));
         }
 
+        [NodeCategory("Create")]
+        public static SP.ElementFilter ElementIntersectElementFilter(DYDB.Element element, bool invert = false)
+        {
+            return new SP.ElementFilter(new ADDB.ElementIntersectsElementFilter(element.InternalElement, invert));
+        }
+
+        [NodeCategory("Create")]
+        public static SP.ElementFilter ElementIntersectSolidFilter(ADDB.Solid solid, bool invert = false)
+        {
+            return new SP.ElementFilter(new ADDB.ElementIntersectsSolidFilter(solid, invert));
+        }
+
+        [NodeCategory("Create")]
+        public static SP.ElementFilter AreaFilter()
+        {
+            return new SP.ElementFilter(new ADDB.AreaFilter());
+        }
+
+        [NodeCategory("Create")]
+        public static SP.ElementFilter AreaTagFilter()
+        {
+            return new SP.ElementFilter(new ADDB.AreaTagFilter());
+        }
+
+        [NodeCategory("Create")]
+        public static SP.ElementFilter RoomFilter()
+        {
+            return new SP.ElementFilter(new ADDB.Architecture.RoomFilter());
+        }
+
+        [NodeCategory("Create")]
+        public static SP.ElementFilter RoomTagFilter()
+        {
+            return new SP.ElementFilter(new ADDB.Architecture.RoomTagFilter());
+        }
+
+        [NodeCategory("Create")]
+        public static SP.ElementFilter CurveElementFilter(ADDB.CurveElementType curveElementType, bool invert = false)
+        {
+            return new SP.ElementFilter(new ADDB.CurveElementFilter(curveElementType, invert));
+        }
+
+        [NodeCategory("Create")]
+        public static SP.ElementFilter FamilyInstanceFilter(ADDB.Document doc, ADDB.ElementId familySymbol)
+        {
+            return new SP.ElementFilter(new ADDB.FamilyInstanceFilter(doc, familySymbol));
+        }
+        
+        // Element Quick Filter //
+        /////////////////////////
     }
 }
